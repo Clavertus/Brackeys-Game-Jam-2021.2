8ff77d4f5f63edb109b8c68a1f58d93eb93d46ab
Rafał Piechnik <ravpiechnik@interia.pl>
2021-08-29 00:13:48 +0200

    Jar Spawner added

---
+++ 2021.2 test room/brackeys 2021.2 test room/Assets/JarSpawner.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JarSpawner : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject jar;
+
+
+    public Transform[] spawnPoints;
+
+
+    public bool jarSpawned;
+
+    public float jarDelayTime;
+
+
+    
+
+    void Awake()
+    {
+        jarSpawned = false;
+        spawnPoints = new Transform[10];
+
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            spawnPoints[i] = transform.GetChild(i);
+        }
+    }
+
+    void Update()
+    {
+        if ((player.GetComponent<PlayerHealth>().health < 50 || player.GetComponent<PlayerHealth>().fuel < 2) && !jarSpawned)
+        {
+         
+                Instantiate(jar, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+                jarSpawned = true;
+                StartCoroutine("Timer");
+            
+
+    
+
+        }
+
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(jarDelayTime);
+        jarSpawned = false;
+    }
+
+}
