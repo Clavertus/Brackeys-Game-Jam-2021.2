ace2a6540185bd7de2e9a5afd2c6f2eb916155fb
Rafał Piechnik <ravpiechnik@interia.pl>
2021-08-28 18:21:00 +0200

    Music in sync! yay

--- 2021.2 test room/brackeys 2021.2 test room/Assets/Scripts/LevelManager.cs
+++ 2021.2 test room/brackeys 2021.2 test room/Assets/Scripts/LevelManager.cs
@@ -14,9 +14,6 @@ public class LevelManager : MonoBehaviour
     // 3 for right
 
     
-
-    float currTimer;
-    public float maxTimer;
     Vector3 currentEuler;
 
      float currentAngle;
@@ -29,41 +26,91 @@ public class LevelManager : MonoBehaviour
     bool rotating;
     public bool u_rotating;
 
+    public int[] time_s;
+    public float[] sampleTimes;
+
+    public float interval;
+    public bool beatStarted;
+
+    public AudioSource levelMusic;
+
     public GameObject player;
 
     void Awake()
     {
         rotating = false;
         u_rotating = false;
+
+        interval = 7.68f;
+        beatStarted = false;
+        time_s = new int[500];
+        sampleTimes = new float[500];
+
+
+        for (int i = 0; i < sampleTimes.Length; i++)
+        {
+            sampleTimes[i] = interval * (i + 1);
+        }
+
+
     }
 
-   
+    void Start()
+    {
+
+
+        for (int i = 0; i < time_s.Length; i++)
+        {
+            time_s[i] = Mathf.RoundToInt(sampleTimes[i] * levelMusic.clip.frequency);
+        }
+
+
+        StartCoroutine("NewUpdate");
+    }
+
+    IEnumerator NewUpdate()
+    {
+        if (beatStarted)
+        {
+            for (var k = 0; k < time_s.Length; k++)
+            {
+                var nSample = time_s[k];
+                while (levelMusic.timeSamples < nSample)
+                {
+                    yield return 0;
+                }
+                 if (currentGrav == 3)
+                 {
+                    currentGrav = 0;
+                    rotating = true;
+                    u_rotating = true;
+                    player.GetComponent<PlayerManager>().PlaySound(player.GetComponent<PlayerManager>().gravityswitchSFX);
+
+                  }
+                 else
+                    {
+                    currentGrav++;
+                    rotating = true;
+                    u_rotating = true;
+                    player.GetComponent<PlayerManager>().PlaySound(player.GetComponent<PlayerManager>().gravityswitchSFX);
+
+                    }
+            }
+        }
+        yield return new WaitForSeconds(Time.deltaTime);
+        StartCoroutine("NewUpdate");
+
+
+
+
+
+
+    }
+
 
     void Update()
     {
 
-
-        currTimer += Time.deltaTime;
-        if (currTimer >= maxTimer)
-        {
-            currTimer = 0;
-            if (currentGrav == 3)
-            {
-                currentGrav = 0;
-                rotating = true;
-                u_rotating = true;
-                player.GetComponent<PlayerManager>().PlaySound(player.GetComponent<PlayerManager>().gravityswitchSFX);
-
-            }
-            else
-            {
-                currentGrav++;
-                rotating = true;
-                u_rotating = true;
-                player.GetComponent<PlayerManager>().PlaySound(player.GetComponent<PlayerManager>().gravityswitchSFX);
-
-            }
-        }
 
         if (rotating)
         {
