4dd9630b712232706934e9d872ab9b1befdde280
Rafał Piechnik <ravpiechnik@interia.pl>
2021-08-28 21:46:16 +0200

    funny ball bounce

---
+++ 2021.2 test room/brackeys 2021.2 test room/Assets/BounceBacc.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceBacc : MonoBehaviour
+{
+
+    [SerializeField] float damage = 40f;
+    string sourceString;
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Player")
+        {
+            Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+            Debug.Log(rb.gameObject.name);
+            Debug.Log("Force received: " + rb.velocity + "   Force applied: " + -rb.velocity.normalized * 3);
+
+            rb.AddForce(-rb.velocity.normalized, ForceMode.Impulse);
+        }
+
+        if (collision.gameObject.GetComponent<PlayerHealth>() && collision.gameObject.GetComponent<PlayerHealth>().health > Mathf.Epsilon)
+        {
+            switch (Random.Range(0, 4))
+            {
+                case 0:
+                    sourceString = "Annihilated by otherwordly creatures";
+                    break;
+
+                case 1:
+                    sourceString = "Bounced back by funny purple ball";
+                    break;
+
+                case 2:
+                    sourceString = "You weren't supposed to touch that.";
+                    break;
+
+                case 3:
+                    sourceString = "Boink!";
+                    break;
+
+                default:
+                    sourceString = "Annihilated by otherwordly creatures";
+                    break;
+            }
+        }
+
+
+        if (collision.gameObject.GetComponent<PlayerHealth>())
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage, sourceString);
+        }
+
+    }
+}
