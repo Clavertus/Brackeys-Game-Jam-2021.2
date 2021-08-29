13d2a4d4528abcc5fe414bee13d2b27e5edf9905
Keegan-Brakhage <88589692+Keegan-Brakhage@users.noreply.github.com>
2021-08-29 04:36:41 -0400

    Level Select

--- 2021.2 test room/brackeys 2021.2 test room/Assets/Scripts/SceneLoader.cs
+++ 2021.2 test room/brackeys 2021.2 test room/Assets/Scripts/SceneLoader.cs
@@ -28,4 +28,25 @@ public class SceneLoader : MonoBehaviour
     {
         Application.Quit();   
     }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
 }
